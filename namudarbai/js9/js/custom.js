9d8d7b9680407adeb64bf7bd789648c24b76e203
Kasparas <kasparasgabe@gmail.com>
2019-08-06 19:01:26 +0300

    Namu darbai

---
+++ namudarbai/js9/js/custom.js
@@ -0,0 +1,11 @@
+// funkcijai turi buti paduotas masyvas
+function spausdintiVardusIrPavardesISarasa(vardai){
+
+	document.write("<ol>");
+
+	for (let i = 0; i < vardai.length; i++){
+		document.write("<li>"+vardai[i]+"</li>")
+	}
+
+	document.write("</ol>");
+}
